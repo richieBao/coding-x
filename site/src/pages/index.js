23d4b1bcb9c9fd1344bd8ad0bc17d22e84ae6f7a
richie bao <richiebao@outlook.com>
2025-10-01 10:33:06 +0800

    Modular Building

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/ue/Modular-Building">
+            to="/docs/ue/Plugin-Modular-Building">
             Modular-Building 🧩🛠️🎮
           </Link>
         </div>
