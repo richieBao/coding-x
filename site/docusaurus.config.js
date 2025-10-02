9bc7a3f4b113c55f91eef86d055535ccb57b57ba
richie bao <richiebao@outlook.com>
2025-10-02 13:34:54 +0800

    Deploy-Export default

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -145,20 +145,3 @@ const config = {
 
 export default config;
 
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-
-export default {
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          path: 'docs',
-          remarkPlugins: [remarkMath],
-          rehypePlugins: [rehypeKatex],
-        },
-      },
-    ],
-  ],
-};
